f2356a2b9bd85cd0d9d8a466abb6ae20f2fc74ff
Priyashis Mojumder <priyashism@gmail.com>
2020-03-04 12:26:18 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+//let pakhi;
+// function add(num1, num2){
+//     console.log(num1+num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const premik = {name: "smart dude", phone:4525358};
+// console.log(premik.bf);
+
+let fun = undefined;
+console.log(fun);
+
+
